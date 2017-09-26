a170c0ebec7003c5ba051e359e30fb193fab6794
Dmytro Prylipko <dmytro.prylipko@hella.com>
2017-09-26 18:41:04 +0200

    Improving train/detect scripts

--- google-tf-models/detect.sh
+++ google-tf-models/detect.sh
@@ -2,15 +2,16 @@
 
 ROOT_DIR=/home/pryldm1/work/google-tf-models
 WORK_DIR=`pwd`
-CONFIG=faster_rcnn_resnet101_01
-ID=2017-09-12_${CONFIG}
+CONFIG=faster_rcnn_inception_resnet_v2_02
+ID=2017-09-21_${CONFIG}
+FOLD=val
 
 EXP_DIR=$WORK_DIR/exp/$ID
 
 export PYTHONPATH=$PYTHONPATH:$ROOT_DIR:$ROOT_DIR/slim
 export CUDA_VISIBLE_DEVICES=0
 
-ITER=74045
+ITER=154108
 if [ ! -f $EXP_DIR/frozen_inference_graph.pb ]; then
 
 cd $ROOT_DIR
@@ -27,6 +28,6 @@ fi
 python3 detect.py --model $EXP_DIR/frozen_inference_graph.pb \
         --label_map data/nexet_label_map.pbtxt \
         --num_classes 6 \
-        --image_dir data/train \
-        --index data/val.csv \
-        --outfile $EXP_DIR/nexet_val_dt_${CONFIG}_${ITER}_google-tf.csv
+        --image_dir data/$FOLD \
+        --index data/$FOLD.csv \
+        --outfile $EXP_DIR/nexet_${FOLD}_dt_${CONFIG}_${ITER}.csv
--- google-tf-models/train.sh
+++ google-tf-models/train.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
+set -e
 
 ROOT_DIR=/home/pryldm1/work/google-tf-models
-WORK_DIR=/home/pryldm1/work/nexar2/google-tf-models
-CONFIG=faster_rcnn_resnet101_01
+WORK_DIR=`pwd`
+CONFIG=faster_rcnn_inception_resnet_v2_02
 EXP_DIR=$WORK_DIR/exp/`date +%Y-%m-%d`_$CONFIG
 
 export PYTHONPATH=$PYTHONPATH:$ROOT_DIR:$ROOT_DIR/slim
@@ -11,14 +12,32 @@ export CUDA_VISIBLE_DEVICES=1
 cd $ROOT_DIR
 mkdir -p $EXP_DIR/train
 mkdir -p $EXP_DIR/eval
+mkdir -p $EXP_DIR/log
 
 python3 object_detection/train.py \
         --logtostderr \
         --pipeline_config_path=$WORK_DIR/configs/$CONFIG.config \
-        --train_dir=$EXP_DIR/train
+        --train_dir=$EXP_DIR/train 2>&1 | tee $EXP_DIR/log/train.log
 
-#python3 object_detection/eval.py \
-#        --logtostderr \
-#        --pipeline_config_path=$WORK_DIR/configs/$CONFIG.config \
-#        --checkpoint_dir=$EXP_DIR/train \
-#        --eval_dir=$EXP_DIR/eval
+
+ITER=`ls -S $EXP_DIR/train/model.ckpt-*.meta | sort -r | grep -oP "(?<=ckpt\-)[0-9]+(?=\.meta)" | head -1`
+
+if [ ! -f $EXP_DIR/frozen_inference_graph.pb ]; then
+
+    cd $ROOT_DIR
+    python3 object_detection/export_inference_graph.py \
+            --input_type image_tensor \
+            --pipeline_config_path $WORK_DIR/configs/$CONFIG.config \
+            --trained_checkpoint_prefix $EXP_DIR/train/model.ckpt-$ITER \
+            --output_directory $EXP_DIR \
+            --optimize_graph
+
+    cd $WORK_DIR
+fi
+
+python3 detect.py --model $EXP_DIR/frozen_inference_graph.pb \
+        --label_map data/nexet_label_map.pbtxt \
+        --num_classes 6 \
+        --image_dir data/train \
+        --index data/val.csv \
+        --outfile $EXP_DIR/nexet_val_dt_${CONFIG}_${ITER}_google-tf.csv
